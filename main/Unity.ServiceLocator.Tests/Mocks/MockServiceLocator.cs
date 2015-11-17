ccdbb20d61030c3723c776ac49e265800645eace
Ariel Szklarkiewicz <ariel.szklarkiewicz@gmail.com>
2015-11-17 16:41:30 -0300

    Namespace renamed

---
+++ main/Unity.ServiceLocator.Tests/Mocks/MockServiceLocator.cs
@@ -0,0 +1,47 @@
+﻿using Unity.ServiceLocator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unity.ServiceLocator.Tests.Mocks
+{
+    class MockServiceLocator : IServiceLocator
+    {
+        public object GetInstance(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetInstance(Type serviceType, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TService GetInstance<TService>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TService GetInstance<TService>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TService> GetAllInstances<TService>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ main/Unity.ServiceLocator.Tests/ServiceLocatorFixture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xunit;
+using Unity.ServiceLocator;
+using Unity.ServiceLocator.Tests.Mocks;
+
+namespace Unity.ServiceLocator.Tests
+{
+    public class ServiceLocatorFixture
+    {
+        public ServiceLocatorFixture()
+        {
+            ServiceLocator.SetLocatorProvider(null);
+        }
+
+        [Fact]
+        public void ServiceLocatorIsLocationProviderSetReturnsTrueWhenSet()
+        {
+            ServiceLocator.SetLocatorProvider(() => new MockServiceLocator());
+
+            Assert.True(ServiceLocator.IsLocationProviderSet);
+        }
+
+        [Fact]
+        public void ServiceLocatorIsLocationProviderSetReturnsFalseWhenNotSet()
+        {
+            Assert.False(ServiceLocator.IsLocationProviderSet);
+        }
+
+        [Fact]
+        public void ServiceLocatorCurrentThrowsWhenLocationProviderNotSet()
+        {
+            IServiceLocator currentServiceLocator;
+            var ex = Assert.Throws<InvalidOperationException>(() => currentServiceLocator = ServiceLocator.Current);
+
+        }
+    }
+}
